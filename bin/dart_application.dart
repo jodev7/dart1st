0822f1428ab7f4cf1bb92c12d98acd71e1509bbd
wheazzydev <joseephnv@gmail.com>
2022-03-21 21:33:19 +0500

    .

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,3 +1,25 @@
 void main() {
-  print('Hello world!');
+  //Numbers     -    num, int, double
+  num n1 = 1.1;
+  print(n1.runtimeType);
+
+  var n2 = 1;
+  print(n2.runtimeType);
+
+  var n3 = 1.3;
+  print(n3.runtimeType);
+
+  //Strings
+
+  var string = 'Number3: ${n3.round()}';
+  print(string);
+
+  //booleans
+
+  var boolean = true;  //bool true, false, null
+  print(boolean);
+
+  dynamic d = 1; //dynamic
+  d = 'd';
+  print(d);
 }
