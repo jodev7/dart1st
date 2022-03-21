1c54dbc0239815d5e20edba53e11db4b90c02aeb
wheazzydev <joseephnv@gmail.com>
2022-03-21 21:45:48 +0500

    .

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,25 +1,32 @@
 void main() {
-  //Numbers     -    num, int, double
-  num n1 = 1.1;
-  print(n1.runtimeType);
+  // //Numbers     -    num, int, double
+  // num n1 = 1.1;
+  // print(n1.runtimeType);
 
-  var n2 = 1;
-  print(n2.runtimeType);
+  // var n2 = 1;
+  // print(n2.runtimeType);
 
-  var n3 = 1.3;
-  print(n3.runtimeType);
+  // var n3 = 1.3;
+  // print(n3.runtimeType);
 
-  //Strings
+  // //Strings
 
-  var string = 'Number3: ${n3.round()}';
-  print(string);
+  // var string = 'Number3: ${n3.round()}';
+  // print(string);
 
-  //booleans
+  // //booleans
 
-  var boolean = true;  //bool true, false, null
-  print(boolean);
+  // var boolean = true;  //bool true, false, null
+  // print(boolean);
 
-  dynamic d = 1; //dynamic
-  d = 'd';
-  print(d);
+  // dynamic d = 1; //dynamic
+  // d = 'd';
+  // print(d);
+
+
+  const hexColor = 0xFFFF0000; //or final
+  print(hexColor);
+
+  final h2 = 0xFFFF0000.abs();
+  print(h2);
 }
